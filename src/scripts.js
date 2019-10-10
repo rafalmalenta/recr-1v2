50699b4a696ba2a63f0fce6d3686b3a0e71de37e
rafalmalenta <rmalenta@wp.pl>
2019-10-10 20:29:07 +0200

    set enviroment for react and enzym testing

---
+++ src/scripts.js
@@ -0,0 +1,9 @@
+import "./styles/style.sass";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Layout from "./Components/Layout";
+
+const app = document.getElementById("app");
+
+ReactDOM.render(<Layout />,app)
